6481cca38ce9728bf372dc90ffcf570a15018ce4
Peter Varkoly <varkoly@suse.com>
2015-06-01 10:31:29 +0200

    Fix copyrights

--- src/clients/auth-client.rb
+++ src/clients/auth-client.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 # ------------------------------------------------------------------------------
-# Copyright (c) 2006-2012 Novell, Inc. All Rights Reserved.
+# Copyright (c) 2014-2015 SUSE Linux GmbH.
 #
 #
 # This program is free software; you can redistribute it and/or modify it under
