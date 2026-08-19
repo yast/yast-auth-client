3ec2dca1cd9cb721e9122582c597c7f9fd20aedf
Noel Power <noel.power@suse.com>
2026-08-19 15:58:29 +0100

    convert remaining net cmd calls to use arglist to bypass shell
    Signed-off-by: Noel Power <noel.power@suse.com>

--- src/lib/auth/authconf.rb
+++ src/lib/auth/authconf.rb
@@ -793,7 +793,8 @@ module Auth
             if !ad_install_samba
                 return ''
             end
-            out, status = Open3.capture2("net ads lookup -S #{ad_host_or_domain}")
+            netcmd = ["net", "ads", "lookup", "-S", "#{ad_host_or_domain}"]
+            out, status = Open3.capture2(*netcmd)
             if status.exitstatus != 0
                 return ''
             end
@@ -814,7 +815,8 @@ module Auth
                 return [false, false]
             end
             smb_conf = ad_create_tmp_smb_conf(ad_domain_name, ad_get_workgroup_name(ad_domain_name))
-            _, status = Open3.capture2("net -s #{smb_conf.path} ads testjoin")
+            netcmd = ["net","-s", "#{smb_conf.path}", "ads", "testjoin"]
+            _, status = Open3.capture2(*netcmd)
             ad_has_computer = status.exitstatus == 0
             klist, _ = Open3.capture2("klist -k")
             kerberos_has_key = klist.split("\n").any?{ |line| /#{Socket.gethostname}.*#{ad_domain_name.downcase}/.match(line.downcase) }
